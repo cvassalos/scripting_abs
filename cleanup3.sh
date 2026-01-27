c877dbfbc8ded39ce261310ef747ee928c329351
Constantinos <vassalce@gmail.com>
2026-01-27 16:04:48 -0500

    Clears log except for last section
    Don't want to wipe out entire system log so this version keeps the last
    section of the message log intact

--- cleanup3.sh
+++ cleanup3.sh
@@ -28,3 +28,30 @@ then
   lines=$1
 else
   lines=$LINES  # Default, if not specified on command-line.
+fi
+
+
+cd $LOG_DIR
+if [ `pwd` != "$LOG_DIR" ]   # or   if [ "$PWD" != "$LOG_DIR" ]
+                              # Not in /var/log?
+then
+  echo "Can't change to $LOG_DIR."
+  exit $E_XCD
+fi  # Doublecheck if in right directory before messing with log file
+
+
+tail -n $lines messages > mesg.temp  #  Save last section of message log file
+mv mesg.temp messages                #  Rename it as system log file 
+
+
+#  cat /dev/null > messages
+#+ No longer needed, as the above method is safer
+
+cat /dev/null > wtmp  #  ': > wtmp' and '> wtmp' have the same effect.
+echo "Log files cleaned up."
+#  Note that there are other log files in /var/log not affected
+#+ by this scripts
+
+exit 0
+#  A zero return value from the script upon exit indicates success
+#+ to the shell
