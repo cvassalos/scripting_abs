ac070d0c5581cbe9c18071657976c5fa20c24923
Constantinos <vassalce@gmail.com>
2026-01-27 20:11:24 -0500

    All needed info is printed
    To complete need to cat to log file

--- ch2/exercise2.sh
+++ ch2/exercise2.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+echo "==DATE=="
 echo $(date)
+echo
 
+echo "==USERS=="
 while IFS= read -r line; do
   echo "$line"
 done < <(who)
+echo
 
+echo "==CURRENT UPTIME=="
 echo $(uptime)
